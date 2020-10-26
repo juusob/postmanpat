a19938987f1b25f6cd67738e96d4f78834a64342
juusob <juuso.saranki@gmail.com>
2020-10-26 21:58:00 +0200

    ok

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo '
+<html>
+<head>
+<title>Postman Pat name generator</title>
+</head>
+<body style="background: white">
+<br/>
+<center><h1>Postman Pat name generator</h1>
+Welcome! Type your name on the field below and click submit to know your Postman Pat name.<br/><br/>
+<form action="result.php" method="post">
+<input name="name">
+</input>
+<br/>
+<br/>
+<input type="submit"/>
+</form>
+</body>
+</html>
+';
+?>
---
+++ result.php
@@ -0,0 +1,14 @@
+<?php
+echo '
+<html>
+<head>
+<title>Postman Pat name generator</title>
+</head>
+<body>
+<center><h1>Postman Pat name generator</h1>
+Congratulations! Your Postman Pat name is:<br/>
+<h2>'.$_POST["name"].' Pat</h2>
+</body>
+</html>
+';
+?>
